72ad584b3e71d7bd68066bcae248b88fb076efc8
Subhasish Saha <sahacst007@gmail.com>
2020-12-30 08:20:44 -0600

    add service worker to work offline

---
+++ sw.js
@@ -0,0 +1,32 @@
+//Service worker
+
+//Cache files
+const cacheName = 'cache-v1';
+const resourcesToPrecache = [
+	'/',
+	'index.html',
+	'styles/main.css'
+];
+
+//Install event handler - called during registration
+//Populate the cache
+self.addEventListener('install', event => {
+	console.log('Service worker install event!');
+	event.waitUntil(
+		caches.open(cacheName).then(cache => {
+			return cache.addAll(resourcesToPrecache);
+		})
+	);
+});
+
+self.addEventListener('activate', event => {
+	console.log('Activate event!');
+});
+
+self.addEventListener('fetch', event => {
+	console.log('Fetch intercepted for:', event.request.url);
+	event.respondWith(caches.match(event.request).then(cachedResponse => {
+		return cachedResponse || fetch(event.request); })
+	);
+});
+
